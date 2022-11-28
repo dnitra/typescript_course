c9780e8fa90e192ab42fa932b6af70ceb7217783
dnitra <109307365+dnitra@users.noreply.github.com>
2022-11-28 18:09:18 +0100

    created and configured ts-config

--- unknown_never_types/dist/app.js
+++ unknown_never_types/dist/app.js
@@ -1,23 +1,19 @@
+"use strict";
 /**
  * uknown type still do some checking...
  * for example weh we try to asign different variable whhich should be specific type to variable with uknown type then error occurs beacuse the the variable with specific type already knows the type it wants
- *  */ 
-let userInput: unknown;
-let userName: string;
-
+ *  */
+let userInput;
+let userName;
 userInput = 5;
 userInput = "Max";
-
 if (typeof userInput === "string") {
     userName = userInput;
 }
-
 /**
  * "never" type means to explicitly  declares, that the function will never return anything
  */
-function generateError(message: string, code: number):never {
-    throw { message:message, errorCode: code}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-
-generateError("An error occurred", 500)
-
+generateError("An error occurred ", 500);
--- unknown_never_types/src/app.ts
+++ unknown_never_types/src/app.ts
@@ -1,15 +1,23 @@
 /**
  * uknown type still do some checking...
  * for example weh we try to asign different variable whhich should be specific type to variable with uknown type then error occurs beacuse the the variable with specific type already knows the type it wants
- *  */
-var userInput;
-var userName;
+ *  */ 
+let userInput: unknown;
+let userName: string;
+
 userInput = 5;
 userInput = "Max";
+
 if (typeof userInput === "string") {
     userName = userInput;
 }
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
+
+/**
+ * "never" type means to explicitly  declares, that the function will never return anything
+ */
+function generateError(message: string, code: number):never {
+    throw { message:message, errorCode: code}
 }
-generateError("An error occurred", 500);
+
+generateError("An error occurred ", 500)
+
