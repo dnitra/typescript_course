9a1fdb498bb9309b6a9733627c8ea4ecc4fccca7
dnitra <109307365+dnitra@users.noreply.github.com>
2022-11-28 12:53:33 +0100

    finished the return types

---
+++ return_types/app.ts
@@ -0,0 +1,37 @@
+function add(n1: number, n2: number) {
+     
+    return n1+n2
+}
+ 
+function printResult(num: number): void {
+    console.log("Result: " + num)
+    
+}
+
+function addAndHandle(
+    n1: number,
+    n2: number,
+    cb: (number:number)=>void
+) {
+    const result = n1 + n2;
+    cb(result)
+}
+
+printResult(add(5, 10));
+
+/**
+ * here we want combineValues to be only a function, which returns a number with two number arguments
+ */
+let combineValues: (
+    a: number,
+    b: number
+) => number;
+
+combineValues = add;
+
+// combineValues = printResult
+console.log(combineValues(8, 8))
+
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+})
